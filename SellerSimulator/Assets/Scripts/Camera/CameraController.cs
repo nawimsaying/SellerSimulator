39928013cf404cbbd6d4e86b894bab0ea17e7ac2
Velp <90183727+Velp6268@users.noreply.github.com>
2023-07-07 15:56:50 +0300

    Remake script camera

--- SellerSimulator/Assets/Scripts/Camera/CameraController.cs
+++ SellerSimulator/Assets/Scripts/Camera/CameraController.cs
@@ -10,6 +10,8 @@ public class CameraController : MonoBehaviour
     public float groundZ = 0; // начальная точка при перемещении камеры
     public Vector3 minBounds; // мин граница координат 
     public Vector3 maxBounds; // макс граница координат
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
     private bool isButtonPressed = false;
 
     private void Update()
@@ -32,6 +34,7 @@ public class CameraController : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 touchStart = GetWorldPosition(groundZ);
+                touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
 
             if (Input.GetMouseButton(0))
@@ -40,8 +43,30 @@ public class CameraController : MonoBehaviour
                 cam.transform.position += direction;
             }
 
+            if(Input.touchCount == 2){
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+                float difference = currentMagnitude - prevMagnitude;
+
+                zoom(difference * 0.01f);
+            }else if(Input.GetMouseButton(0)){
+                Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Camera.main.transform.position += direction;
+            }
+             zoom(Input.GetAxis("Mouse ScrollWheel"));
+
             LimitCameraPosition();
         }
+    }
+
+    private void zoom(float increment){
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 
     private Vector3 GetWorldPosition(float z)
