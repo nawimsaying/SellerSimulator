477eff611e68259595bea37e99c45bae237b4e43
xorti <yranidrop@gmail.com>
2023-07-15 15:29:15 +0300

    make dinamyc resolution

--- SellerSimulator/Assets/Scripts/SceneSettings.cs
+++ SellerSimulator/Assets/Scripts/SceneSettings.cs
@@ -7,17 +7,38 @@ public class SceneSettings : MonoBehaviour
 {
     [SerializeField] private Text _fpsText;
     [SerializeField] private bool _isFpsCounterEnable;
+    [SerializeField] private Text screenText;
 
     private float _fps;
 
+    // Динамическое разрешение
+    private Vector2 _mainResolution;
+    private float _currentScale = 1;
+    private float _minScale = 0.5f;
+    private float _scaleStep = 0.05f;
+    private int _minFps = 30;
+    private int _maxFps = 60;
+    private float _minFpsFloat;
+    private float _maxFpsFloat;
+    private float _delay;
+    private float _delayTime;
+
+
     void Start()
     {
+        _mainResolution = new Vector2(Screen.width, Screen.height);
+        _delayTime = _delay;
+        _minFpsFloat = 1f / (float)_minFps;
+        _maxFpsFloat = 1f / (float)_maxFps;
+
+        // По умолчанию отключаем текст с фпс
         _fpsText.gameObject.active = false;
 
+        // Устанавливаем пресет графики и ограничение фпс в зависимости от платформы
 #if UNITY_EDITOR || UNITY_STANDALONE
         SetTestPreset();
 #else
-        SetNormalPreset();
+        SetEcoPreset();
 #endif
     }
 
@@ -37,6 +58,30 @@ public class SceneSettings : MonoBehaviour
             if (_fpsText.IsActive())
                 _fpsText.gameObject.active = false;
         }
+
+        // Динамическое разрешение
+        if (Time.time > _delayTime)
+        {
+            // Уменьшение разрешения
+            if (Time.deltaTime > _minFpsFloat)
+            {
+                if (_currentScale > _minScale)
+                {
+                    _currentScale -= _scaleStep;
+                    Screen.SetResolution((int)(_mainResolution.x * _currentScale), (int)(_mainResolution.y * _currentScale), true);
+                    _delayTime = Time.time + _delay;
+                }
+            }
+            // Увеличение разрешения
+            /*else if (CurScale < 1 && Time.deltaTime < MaxFPSS)
+            {
+                CurScale += ScaleStep;
+                Screen.SetResolution((int)(MainRes.x * CurScale), (int)(MainRes.y * CurScale), true);
+                DelayTime = Time.time + Delay;
+            }*/
+            _delayTime = Time.time + 0.5f;
+        }
+        screenText.text = "MainRes: " + _mainResolution + "X: " + Screen.width + " / Y: " + Screen.height + " / CurScale " + _currentScale + " / ";
     }
 
     public static void ChangeGraphicSettings(bool isEcoModeEnable)
