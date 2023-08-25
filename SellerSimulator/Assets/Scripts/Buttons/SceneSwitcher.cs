d0ecd3583684a6c3460b24969da53764718aee51
xorti <yranidrop@gmail.com>
2023-08-25 22:11:43 +0300

    add ui in room scene

--- SellerSimulator/Assets/Scripts/Buttons/SceneSwitcher.cs
+++ SellerSimulator/Assets/Scripts/Buttons/SceneSwitcher.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    [SerializeField] private Animation transitionIn;
-
     public void SceneSwitch(int _idScene)
     {
-        transitionIn.Play();
         SceneManager.LoadScene(_idScene);
     }
 }
